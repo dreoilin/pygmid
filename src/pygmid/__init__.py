a493369c96d956d9ec05cd356f71cd566f7d3937
madrasalach <cian.odonnell@ucdconnect.ie>
2023-01-06 19:54:36 +0000

    Added interp function
    Interp function is a new import that aims to incorporate PchipInterpolator into standard interp1d

--- src/pygmid/__init__.py
+++ src/pygmid/__init__.py
@@ -2,5 +2,6 @@ from .version import __version__
 from .Lookup import *
 from .Sweep import *
 from .pygmid import main
+from .numerical import interp1
 
-__all__ = ['main', 'Lookup', 'Sweep']
+__all__ = ['main', 'Lookup', 'Sweep', 'interp1']
