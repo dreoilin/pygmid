ecbe6213bc0f874f8d9268856e107a56dc6c47b4
Tiarnach <tiarnachreidy@gmail.com>
2023-07-10 16:55:27 +0100

    Added tests for lookup function

--- tests/pygmid/test_lookup.py
+++ tests/pygmid/test_lookup.py
@@ -5,8 +5,12 @@ import scipy
 
 from pygmid import Lookup
 
+@pytest.fixture(scope='module')
+def expected_data() -> dict:
+    return scipy.io.loadmat('tests/files/gen_test_lookup_data.mat', matlab_compatible=True)
+
 def test_lookup_alias(nch):
-    VDSs = nch['VDS']       # lookup object has pseudo-array access to data
+    VDSs = nch['VDS']
     VGSs = np.arange(0.4, 0.6, 0.05)
 
     IDe = nch.look_up('ID', vds=VDSs, vgs=VGSs)
@@ -15,30 +19,68 @@ def test_lookup_alias(nch):
     assert IDe is not None and len(IDe)>0
     assert np.testing.assert_array_equal(IDe, IDa) is None
 
-# TODO
-def test_look_up(nch: Lookup):
-#     VDSs = nch['VDS']       # lookup object has pseudo-array access to data
-#     VGSs = np.arange(0.4, 0.6, 0.05)
+def test_lookupVGS_alias(nch: Lookup):
+    expected_vgs = nch.look_upVGS(GM_ID = 10, VDS = 0.6, VSB = 0.1, L = 0.18)
+    actual_vgs = nch.lookupVGS(GM_ID = 10, VDS = 0.6, VSB = 0.1, L = 0.18)
+    assert np.allclose(actual_vgs, expected_vgs)
 
-#     # Plot ID versus VDS
-#     ID = nch.look_up('ID', vds=VDSs, vgs=VGSs)
-    # CHECKME: A rather silly example
-    assert nch.look_up('A', a_b='A_B') == []
 
+def test_look_up(nch: Lookup, expected_data):
+
+    expected_1_ID       = expected_data['expected_1_ID']
+    expected_2_vt       = expected_data['expected_2_vt']
+    expected_3_ft       = expected_data['expected_3_ft']
+    expected_4_id_w     = expected_data['expected_4_id_w']
+    expected_5_id_w     = expected_data['expected_5_id_w']
+    expected_6_gm_gds   = expected_data['expected_6_gm_gds']
+    expected_7_gm_ID    = expected_data['expected_7_gm_ID']
+
+    # TODO CHECKME: A rather silly example
+    assert len(nch.look_up('A', a_b='A_B')) == 0
+
+    arange_endpoint = lambda start, stop, step: np.arange(start, stop+step, step)
+    # 1
+    r_vds = nch['VDS'] 
+    r_vgs = arange_endpoint(0.4, 0.6, 0.05)
+    actual_1_ID = nch.look_up('ID', vds=r_vds, vgs=r_vgs)
+    assert np.allclose(actual_1_ID, expected_1_ID)
+
+    # 2
+    Ls = nch['L']
+    actual_2_vt = nch.look_up('VT', vgs=0.6, L=Ls)
+    assert np.allclose(actual_2_vt.flatten(), expected_2_vt.flatten())
+
+    # 3
+    gm_ids = arange_endpoint(5, 20, 0.1)
+    Ls = np.arange(min(nch['L']),0.3,0.05)
+    actual_3_ft = nch.look_up('GM_CGG', GM_ID=gm_ids, L =np.arange(min(Ls),0.3,0.05))/2/np.pi
+    assert np.allclose(actual_3_ft, expected_3_ft)
+
+    # 4
+    gm_ids = arange_endpoint(5, 20, 0.1)
+    Ls = [0.18, 0.23, 0.28, 0.3]
+    actual_4_id_w = nch.look_up('ID_W', GM_ID=gm_ids, L=Ls)
+    assert np.allclose(actual_4_id_w, expected_4_id_w)
+
+    # 5
+    gm_ids = arange_endpoint(5, 20, 0.1)
+    actual_5_id_w = nch.look_up('ID_W', GM_ID=gm_ids, VDS=[0.8, 1.0, 1.2])
+    assert np.allclose(actual_5_id_w, expected_5_id_w)
+
+    # 6
+    gm_ids = arange_endpoint(5, 20, 0.1)
+    actual_6_gm_gds = nch.look_up('GM_GDS', GM_ID=gm_ids)
+    assert np.allclose(actual_6_gm_gds, expected_6_gm_gds.flatten())
+
+    # 7
+    actual_7_gm_ID = nch.look_up('GM_ID', VDS=arange_endpoint(0.025, 1.2, 0.025), VSB=0.0, L=0.18)
+    assert np.allclose(actual_7_gm_ID, expected_7_gm_ID)
 
 def test___contains__(nch: Lookup):
     assert 'L' in nch
     assert 'VGS' in nch
     assert 'VDS' in nch
     assert 'VSB' in nch
-
-    assert 'METHOD' not in nch
-    
-    # Missing
-    assert 'VGB' not in nch
-    assert 'GM_ID' not in nch
-    assert 'ID_W' not in nch
-    assert 'VDB' not in nch
 
     # methods and properties of the object not included
     assert '__init__' not in nch
@@ -48,11 +90,6 @@ def test___getitem__(nch: Lookup):
     assert nch['VGS'] is not None
     assert nch['VDS'] is not None
     assert nch['VSB'] is not None
-    # CHECKME: these should not be in the data?
-    # assert nch['VGB']
-    # assert nch['GM_ID']
-    # assert nch['ID_W']
-    # assert nch['VDB']
 
     with pytest.raises(ValueError):
         assert nch['nonexistingkey']
