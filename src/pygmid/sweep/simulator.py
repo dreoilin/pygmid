7c9bad2470dcdcab35194b6567cbaf3369431c55
Tiarnach <tiarnachreidy@gmail.com>
2023-07-21 10:46:18 +0100

    Uncommented simulator code

--- src/pygmid/sweep/simulator.py
+++ src/pygmid/sweep/simulator.py
@@ -5,13 +5,11 @@ class SpectreSimulator:
         self.__args = args
     
     def run(self, filename: str):
-        return
-        # TODO: uncomment this to run the simulation
-        # infile = filename
-        # try:
-        #     cmd_args = ['spectre', filename] + [*self.__args]
-        #     cp = subprocess.run(cmd_args, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # except subprocess.CalledProcessError as e:
-        #     logging.info(f"Error executing process\n\n{e}")
-        #     return
-        # logging.info(cp.stdout)
+        infile = filename
+        try:
+            cmd_args = ['spectre', filename] + [*self.__args]
+            cp = subprocess.run(cmd_args, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            logging.info(f"Error executing process\n\n{e}")
+            return
+        logging.info(cp.stdout)
